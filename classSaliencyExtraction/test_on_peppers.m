284b50b5785e63318d3d37257f06547e4212b751
JeffOwOSun <sunyushi9546@gmail.com>
2015-07-30 14:44:45 +0800

    improve test script to include sections and projection

--- classSaliencyExtraction/test_on_peppers.m
+++ classSaliencyExtraction/test_on_peppers.m
@@ -1,14 +1,15 @@
+%% setup matconvnet and vlfeat
 run setup.m;
 
-% load network
+%% load network
 net_vgg = 'imagenet-vgg-f.mat';
 net_alex = 'imagenet-caffe-alex.mat';
 % I choose alex
-net = load(net_alex);
+net = load(net_vgg);
 
-% load Image
-% number of desired image
-imno = 1;
+%% load Image
+% index of desired image
+imno = 2;
 load('imdb.mat');
 im = imread(imdb(imno).name);
 % pre-process image
@@ -16,23 +17,28 @@ im_ = single(im);
 im_ = imresize(im_, net.normalization.imageSize(1:2));
 im_ = im_ - net.normalization.averageImage;
 
-% extract saliency
-layerno = 16; % look at the INPUT of 16-th layer
+%% set layernumber
+layerno = 2; % look at the INPUT of 16-th layer
+
+%% extract saliency
 [saliency_map, feature_map] = saliency_extraction(net, im_, imdb(imno).class, layerno); % a map for each channel
 
-% plot the average of the activation
-saliency_absol = abs(saliency_map);
-saliency_absol = max(saliency_absol,[],3);
-% take the norm of the 3D saliency map for each pixel
-saliency_norm = sqrt(sum(saliency_map .^ 2,3));
+%% project the feature maps
+projected_fmap = feature_map_projection(net, im_, layerno);
 
+%% calculate the average of the activation
+% saliency_absol = abs(saliency_map);
+% saliency_absol = max(saliency_absol,[],3);
+% % take the norm of the 3D saliency map for each pixel
+% saliency_norm = sqrt(sum(saliency_map .^ 2,3));
+
+%% display original image
 figure(1); clf;
-subplot(2,2,1); imagesc(im); title('original');
-subplot(2,2,2); imagesc(saliency_norm); title('saliency norm'); colormap gray; colorbar;
-subplot(2,2,3); imagesc(saliency_absol); title('saliency absol'); colormap gray; colorbar;
+imagesc(im); title('original');
 
-% plot the tile of the activation
+%% plot the results
 figure(2); clf; 
 subplot(2,2,1); vl_imarraysc(saliency_map); colormap gray; title('saliency maps');
 subplot(2,2,2); vl_imarraysc(feature_map); colormap gray; title('feature maps');
-subplot(2,2,3); imagesc(im); title('original');
+subplot(2,2,3); vl_imarraysc(projected_fmap); title('fmap projection');
+subplot(2,2,4); imagesc(im); title('original');
