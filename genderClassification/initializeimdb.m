fe66abd62b6ec353f452f704a46878c76aa1b8b9
JeffOwOSun <sunyushi9546@gmail.com>
2015-08-15 19:45:03 +0800

    change labels; round the train val division point; change normalization algorithm to use net-embedded average

--- genderClassification/initializeimdb.m
+++ genderClassification/initializeimdb.m
@@ -2,39 +2,39 @@ function imdb = initializeimdb
 
 %% setup
 setup
-
 % set dir
 female_dir = 'lfw-gender/female';
 male_dir = 'lfw-gender/male';
-
 %% load images
 female_imarray = load_jpg_dir(female_dir);
 male_imarray = load_jpg_dir(male_dir);
 %% even the number of images
 % here first param for randperm is the range it permutes
 % the second param is the number of indices it should return
-female_imarray = female_imarray{randperm(numel(female_imarray), numel(male_imarray))};
-
+male_imarray = male_imarray(randperm(numel(male_imarray), numel(female_imarray)));
 %% set labels
-% female -> 0
-% male -> 1
-female_labels = zeros(1, numel(female_imarray));
-male_labels = ones(1, numel(male_imarray)); 
+% female -> 1
+% male -> 2
+female_labels = ones(1, numel(female_imarray), 'single');
+male_labels = 2*ones(1, numel(male_imarray), 'single'); 
 labels = [female_labels male_labels];
 %% set set
-female_set = ones(1, numel(female_imarray));
+female_set = ones(1, numel(female_imarray), 'single');
 val_index = randperm(numel(female_set)); % index of validation sets
-val_index = val_index(1:numel(val_index)/5);
+val_index = val_index(round(1:numel(val_index)/5));
 female_set(val_index) = 2; % validation set
-male_set = ones(1, numel(male_imarray));
+male_set = ones(1, numel(male_imarray), 'single');
 val_index = randperm(numel(male_set));
-val_index = val_index(1:numel(val_index)/5);
+val_index = val_index(round(1:numel(val_index)/5));
 male_set(val_index) = 2;
 set = [female_set, male_set];
 %% normalize images
-data = cat(4, female_imarray{:} ,male_imarray{:});
-dataMean = mean(data(:,:,:,set == 1), 4);
-data = bsxfun(@minus, data, dataMean);
+data = cat(2, female_imarray, male_imarray);
+data = cellfun(@(x)imresize(x,[224,224]), data, 'UniformOutput', false);
+data = cat(4, data{:}); % peel the cell array
+% dataMean = mean(data(:));
+% data = bsxfun(@minus, data, dataMean);
+% data = data - dataMean;
 %% set the return values
 imdb.images.data = data;
 imdb.images.labels = labels;
