2a30380543a98cc489ee0fb6dc21e49c5f1f675a
JeffOwOSun <sunyushi9546@gmail.com>
2015-08-15 20:39:12 +0800

    correct classes when initializing

--- genderClassification/initialize_net.m
+++ genderClassification/initialize_net.m
@@ -8,3 +8,7 @@ net = load('imagenet-vgg-f.mat');
 % replace the last layers
 net.layers{20}.weights={0.01/opts.scale * randn(1, 1, 4096, 2, 'single'), []};
 net.layers{21} = struct('type', 'softmaxloss', 'name', 'loss');
+
+% set the classes
+net.classes.name = {'female', 'male'};
+net.description = {'female', 'male'};
